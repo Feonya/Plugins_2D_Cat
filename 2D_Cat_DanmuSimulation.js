7fbb44584a5eb723413c7751181fea0e2a9089ab
tuxyin <tuxyin@163.com>
2021-08-28 12:49:04 +0800

    添加DanmuSimulation

---
+++ 2D_Cat_DanmuSimulation.js
@@ -0,0 +1,227 @@
+/*:
+ * @target MZ
+ * @plugindesc 在游戏画面模拟弹幕效果。v1.0
+ * @author 2D_猫
+ * @url https://space.bilibili.com/137028995
+ *
+ * @help
+ * * 使用方法：在任意事件中调用本插件的“开启弹幕”指令，并依次设置好相关参数。
+ *
+ * * 使用条款：免费用于任何商业或非商业目的；允许在保留原作者信息的前提下修改代
+ * 码；请在你的项目中致谢“2D_猫”，谢谢！:)
+ *
+ * * 更新日志：
+ * -- 20210827 v1.0
+ *     实现插件基本功能。
+ *
+ * @command openDanmu
+ * @text 开启弹幕
+ *
+ * @arg danmuStrings
+ * @text 弹幕文字
+ * @type string[]
+ * @default ["666~~[5]", "跳的真好看！[4]", "老铁们，礼物刷起来啊~~！[3]", "哦耶~~[2]", "射射，已经谢了。。。[1]", "2333[5]"]
+ * @desc 每行设置一条弹幕，中括号内表示该弹幕出现的权重（任意正数），越高出现概率越大，反之越小。中括号不能出现于其他地方。
+ *
+ * @arg maxFontSize
+ * @text 弹幕最大字号
+ * @type number
+ * @default 36
+ * @min 0
+ *
+ * @arg minFontSize
+ * @text 弹幕最小字号
+ * @type number
+ * @default 24
+ * @min 0
+ *
+ * @arg maxMoveSpeed
+ * @text 弹幕最大速度
+ * @type number
+ * @default 300
+ * @min 1
+ *
+ * @arg minMoveSpeed
+ * @text 弹幕最小速度
+ * @type number
+ * @default 150
+ * @min 1
+ *
+ * @arg totalGenerateDuration
+ * @text 总弹幕生成持续时间（毫秒）
+ * @type number
+ * @default 10000
+ * @min 0
+ *
+ * @arg maxGenerateDuration
+ * @text 每条弹幕生成最大间隔（毫秒）
+ * @type number
+ * @default 100
+ * @min 0
+ *
+ * @arg minGenerateDuration
+ * @text 每条弹幕生成最小间隔（毫秒）
+ * @type number
+ * @default 0
+ * @min 0
+ *
+ * @arg colors
+ * @text 色彩设置（带“#”的16进制值）
+ * @type string[]
+ * @default ["#ffffff[5]", "#ff0000[2]", "#ff00ff[2]", "#0000ff[2]", "#00ff00[2]", "#fff000[2]"]
+ * @desc 每行设置一种色彩，中括号内表示该颜色出现的权重（任意正数），越高出现概率越大，反之越小。中括号不能出现于其他地方。
+ *
+ * @arg upPosY
+ * @text 弹幕显示最高位置（px）
+ * @type number
+ * @min 0
+ * @default 0
+ *
+ * @arg downPosY
+ * @text 弹幕显示最低位置（px）
+ * @type number
+ * @min 0
+ * @default 550
+ */
+
+(() => {
+    class Danmu {
+        constructor(str, fnSize, mvSpd, color) {
+            this.str    = str;
+            this.fnSize = fnSize;
+            this.mvSpd  = mvSpd;
+            this.color  = color;
+            this.msg    = null; // Pixi的Text对象
+        }
+    }
+
+    var danmusOnStage = [];  // 舞台上的所有弹幕
+
+    var danmuStrs    = []; // [['弹幕1', '权重1'], ['弹幕2', '权重2'], ...]
+    var maxFnSize    = 0;
+    var minFnSize    = 0;
+    var maxMvSpd     = 0;
+    var minMvSpd     = 0;
+    var totalGenDura = 0;
+    var maxGenDura   = 0;
+    var minGenDura   = 0;
+    var colors       = []; // [['色彩1', '权重1'], ['色彩2', '权重2'], ...]
+    var upPosY       = 0;
+    var downPosY     = 0;
+
+    var isOn = false;
+    var nextGenDura   = 0;
+    var currTime      = 0; // 生成下一条弹幕之前的积累时间
+    var currTotalTime = 0; // 结束所有弹幕之前的积累时间
+    var waitForDone   = false;
+
+    PluginManager.registerCommand('2D_Cat_DanmuSimulation', 'openDanmu', args => {
+        if (isOn) return;
+        isOn = true;
+
+        danmuStrs    = parseStrArr(JSON.parse(args.danmuStrings));
+        maxFnSize    = Number(args.maxFontSize);
+        minFnSize    = Number(args.minFontSize);
+        maxMvSpd     = Number(args.maxMoveSpeed);
+        minMvSpd     = Number(args.minMoveSpeed);
+        totalGenDura = Number(args.totalGenerateDuration);
+        maxGenDura   = Number(args.maxGenerateDuration);
+        minGenDura   = Number(args.minGenerateDuration);
+        colors       = parseStrArr(JSON.parse(args.colors));
+        upPosY       = Number(args.upPosY);
+        downPosY     = Number(args.downPosY);
+
+        Graphics.app.ticker.add(gameLoop);
+    });
+
+    function gameLoop() {
+        // 判断是否生成新的弹幕
+        if (!waitForDone) {
+            currTime += Graphics.app.ticker.deltaMS;
+            if (currTime >= nextGenDura) {
+                danmusOnStage.push(GenADanmu());
+                currTime    = 0;
+                nextGenDura = Math.floor(Math.random() * (maxGenDura + 1 - minGenDura) + minGenDura);
+            }
+        }
+
+        // 移动舞台上现有弹幕
+        danmusOnStage.forEach(e => {
+           e.msg.x -= e.mvSpd * (Graphics.app.ticker.deltaMS * 0.001);
+
+           // 判断是否销毁弹幕
+           if (e.msg.x + e.msg.width < 0) {
+               let idx = danmusOnStage.indexOf(e);
+               let dm  = danmusOnStage.splice(idx, 1);
+               Graphics.app.stage.removeChild(dm);
+           }
+        });
+
+        // 判断是否停止所有弹幕的生成
+        currTotalTime += Graphics.app.ticker.deltaMS;
+        if (currTotalTime >= totalGenDura) {
+            waitForDone = true;
+        }
+
+        // 判断是否可以结束本次弹幕
+        if (waitForDone && danmusOnStage.length === 0) {
+            Graphics.app.ticker.remove(gameLoop);
+            isOn          = false;
+            nextGenDura   = 0;
+            currTime      = 0;
+            currTotalTime = 0;
+            waitForDone   = false;
+        }
+    }
+
+    function parseStrArr(oriArr) {
+        let arr = [];
+        oriArr.forEach(e => {
+            let a = e.split('[');
+            a[1]  = a[1].slice(0, -1); // 去掉']'
+            arr.push(a);
+        });
+
+        return arr;
+    }
+
+    function randSelAStrFromArr(oriArr) {
+        let strArr = [];
+        let weiArr = [];
+        let totalWei = 0;
+
+        oriArr.forEach(e => {
+            strArr.push(e[0]);
+            let wei = Number(e[1]);
+            weiArr.push(wei);
+            totalWei += wei;
+        });
+
+        let randNum = Math.random() * totalWei;
+        console.log(randNum)
+        let weiAccumulating = 0;
+        for (let i = 0; i < weiArr.length; i++) {
+            weiAccumulating += weiArr[i];
+            if (randNum < weiAccumulating) {
+                return strArr[i];
+            }
+        }
+
+        return strArr[strArr.length - 1];
+    }
+
+    function GenADanmu() {
+        let danmu = new Danmu(
+            randSelAStrFromArr(danmuStrs),
+            Math.floor(Math.random() * (maxFnSize + 1 - minFnSize) + minFnSize),
+            Math.floor(Math.random() * (maxMvSpd  + 1 - minMvSpd)  + minMvSpd),
+            randSelAStrFromArr(colors)
+        );
+
+        danmu.msg = new PIXI.Text(danmu.str, new PIXI.TextStyle({fontSize: danmu.fnSize, fill: danmu.color}));
+        danmu.msg.position.set(Graphics.app.stage.children[0].width, Math.random() * (downPosY - upPosY) + upPosY);
+        Graphics.app.stage.addChild(danmu.msg);
+
+        return danmu;
+    }
+})();
