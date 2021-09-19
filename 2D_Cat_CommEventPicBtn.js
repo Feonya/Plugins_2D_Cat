1fdc2ae98f56709b87001e93e04ecfb69378b5d0
tuxyin <tuxyin@163.com>
2021-09-20 00:17:52 +0800

    v1.4

--- 2D_Cat_CommEventPicBtn.js
+++ 2D_Cat_CommEventPicBtn.js
@@ -1,6 +1,6 @@
 /*:
  * @target     MZ
- * @plugindesc v1.3 调出一个或多个自定义图片按钮，点击触发共通事件。
+ * @plugindesc v1.4 调出一个或多个自定义图片按钮，点击触发共通事件。
  * @author     2D_猫
  * @url        https://space.bilibili.com/137028995
  *
@@ -9,12 +9,20 @@
  * 1、需要使用的图片请放置在img/pictures目录下，仅支持png图片。
  * 2、在任意事件中调用本插件的“调出图片按钮”指令，并依次设置好相关参数即可。
  * 3、在执行内容未等待情况下，多次调用上方指令，可同时调出多个图片按钮。
- * 4、点击任意图片按钮后，将会移除当前正在显示的所有图片按钮。
+ * 4、可以在任意事件中调用“销毁图片按钮”来销毁指定编号的图片按钮，所有拥有该编
+ * 号的图片按钮将被销毁。
+ * 5、可以在任意事件中调用“销毁所有图片按钮”一次性销毁现存所有图片按钮。
  *
  * * 使用条款：免费用于任何商业或非商业目的；允许在保留原作者信息的前提下修改代
  * 码；请在你的项目中致谢“2D_猫”，谢谢！:)
  *
  * * 更新日志：
+ * -- 20210919 v1.4
+ *     图片按钮不会再遮挡图片以及对话框等窗口。
+ *     对话框弹出时，图片按钮不可点击。
+ *     图片按钮存在时可以调出菜单界面，返回后恢复图片按钮。
+ *     增加了“销毁图片按钮”和“销毁所有图片按钮”指令，现在点击图片按钮不会消失，
+ * 需要调用指令手动销毁。
  * -- 20210917 v1.3
  *     增加了设置图片锚点位置的指令；图片按钮坐标值现在可以设为负值。
  * -- 20210915 v1.2
@@ -46,13 +54,23 @@
  * @text    调出图片按钮
  * @desc    非等待情况下，多次调用本指令可同时调出多个按钮。
  *
+ * @arg     buttonId
+ * @text    图片按钮编号
+ * @type    number
+ * @default 1
+ * @min     1
+ *
+ * @arg     _cutLine1
+ * @text    ------------------------
+ * @default
+ *
  * @arg     buttonNormalPictureName
  * @text    正常按钮图片
  * @desc    至少要设置一个正常按钮图片。
  * @type    file
  * @dir     img/pictures
  *
- * @arg     _cutLine1
+ * @arg     _cutLine2
  * @text    ------------------------
  * @default
  *
@@ -74,7 +92,7 @@
  * @default 1.5
  * @desc    可选，0~10之间的实数，0为纯黑，1为正常，10为最亮，若留空，则悬停时不改变图片亮度。
  *
- * @arg     _cutLine2
+ * @arg     _cutLine3
  * @text    ------------------------
  * @default
  *
@@ -96,7 +114,7 @@
  * @default 0.5
  * @desc    可选，0~10之间的实数，0为纯黑，1为正常，10为最亮，若留空，则按下时不改变图片亮度。
  *
- * @arg     _cutLine3
+ * @arg     _cutLine4
  * @text    ------------------------
  * @default
  *
@@ -112,7 +130,7 @@
  * @default 0.5
  * @desc    0~1之间的实数，越大锚点越靠图片下边，反之越靠上边，0.5为图片y轴中心点。
  *
- * @arg     _cutLine4
+ * @arg     _cutLine5
  * @text    ------------------------
  * @default
  *
@@ -132,7 +150,7 @@
  * @max     50000
  * @desc    即图片按钮显示在游戏画面中的y坐标值。
  *
- * @arg     _cutLine5
+ * @arg     _cutLine6
  * @text    ------------------------
  * @default
  *
@@ -143,7 +161,7 @@
  * @min     0
  * @desc    按下按钮后调用哪个共通事件，0表示不调用任何共通事件。
  *
- * @arg     _cutLine6
+ * @arg     _cutLine7
  * @text    ------------------------
  * @default
  *
@@ -151,13 +169,26 @@
  * @text    玩家是否可移动
  * @type    boolean
  * @default false
- * @desc    图片按钮显示时玩家是否可移动。
+ * @desc    图片按钮显示时玩家是否可移动，注意本参数最后一次设定将会影响之前本参数的设定。
  *
  * @arg     ignoreTransparence
  * @text    是否忽略透明像素
  * @type    boolean
  * @default true
  * @desc    点击图片按钮时是否忽略透明部分，注意本参数最后一次设定将会影响之前本参数的设定。
+ *
+ * @command destroyButtonById
+ * @text    销毁图片按钮
+ *
+ * @arg     destroyButtonId
+ * @text    图片按钮编号
+ * @type    number
+ * @default 1
+ * @min     1
+ * @desc    销毁所有此编号的图片按钮。
+ *
+ * @command destroyAllButtons
+ * @text    销毁所有图片按钮
  */
 
 var P_2D_C = P_2D_C || {};
@@ -166,6 +197,7 @@ var P_2D_C = P_2D_C || {};
     class CommEventPicBtn {
         constructor() {
             this.spr      = null;
+            this.id       = 1;
             this.norTex   = null;
             this.overTex  = null;
             this.overTint = null;
@@ -187,7 +219,8 @@ var P_2D_C = P_2D_C || {};
         }
 
         onBtnDown() {
-            console.log('down')
+            // console.log('down')
+            if ($gameMessage.isBusy()) return;
             if (this.downTex  !== null) this.spr.texture = this.downTex;
             if (this.downTint !== null) this.spr.tint    = this.downTint;
             if (this.downBrit !== null) this.spr.filters = [this.downBrit];
@@ -195,7 +228,8 @@ var P_2D_C = P_2D_C || {};
         }
 
         onBtnUp() {
-            console.log('up')
+            // console.log('up')
+            if ($gameMessage.isBusy()) return;
             if (this.isOver) {
                 if (this.overTex  !== null) this.spr.texture = this.overTex;
                 if (this.overTint !== null) this.spr.tint    = this.overTint;
@@ -206,15 +240,16 @@ var P_2D_C = P_2D_C || {};
                 this.spr.filters = [];
             }
             if (this.isDown) {
-                destoryAllBtnSpr();
+                // destoryAllBtnSpr();
                 if (this.commEvId !== 0) $gameTemp.reserveCommonEvent(this.commEvId);
             }
             this.isDown  = false;
         }
 
         onBtnOver() {
-            console.log('over')
-            if (this.isDown) return;
+            // console.log('over')
+            if ($gameMessage.isBusy()) return;
+            if (this.isDown)           return;
             if (this.overTex  !== null) this.spr.texture = this.overTex;
             if (this.overTint !== null) this.spr.tint    = this.overTint;
             if (this.overBrit !== null) this.spr.filters = [this.overBrit];
@@ -222,8 +257,9 @@ var P_2D_C = P_2D_C || {};
         }
 
         onBtnOut() {
-            console.log('out')
-            if (this.isDown) return;
+            // console.log('out')
+            if ($gameMessage.isBusy()) return;
+            if (this.isDown)           return;
             this.spr.texture = this.norTex;
             this.spr.tint    = 0xffffff;
             this.spr.filters = [];
@@ -232,20 +268,21 @@ var P_2D_C = P_2D_C || {};
 
     }
 
-    var button    = null;
-    var btnSprArr = [];
-    var _Game_Player_prototype_canMove = Game_Player.prototype.canMove;
-    var _Scene_Map_prototype_callMenu  = Scene_Map  .prototype.callMenu;
+    var button = null;
+    var _Game_Player_prototype_canMove       = Game_Player.prototype.canMove;
     var _PIXI_Sprite_prototype_containsPoint = PIXI.Sprite.prototype.containsPoint;
 
+    P_2D_C.picBtnContainer = null;
+    P_2D_C.picBtnsArr      = [];
+    P_2D_C.pixiTempApp     = P_2D_C.pixiTempApp || new PIXI.Application();
+
     PluginManager.registerCommand('2D_Cat_CommEventPicBtn', 'showPictureButton', args => {
+        P_2D_C.btnId          = Number(args.buttonId);
         P_2D_C.btnNorPicName  = String(args.buttonNormalPictureName);
         P_2D_C.btnOverPicName = String(args.buttonOverPictureName);
-        // P_2D_C.btnOverPicTint = String(args.buttonOverPictureTint);
         P_2D_C.btnOverPicTint = Number('0x' + args.buttonOverPictureTint.slice(1));
         P_2D_C.btnOverPicBrit = Number(args.buttonOverPictureBrightness);
         P_2D_C.btnDownPicName = String(args.buttonDownPictureName);
-        // P_2D_C.btnDownPicTint = String(args.buttonDownPictureTint);
         P_2D_C.btnDownPicTint = Number('0x' + args.buttonDownPictureTint.slice(1));
         P_2D_C.btnDownPicBrit = Number(args.buttonDownPictureBrightness);
         P_2D_C.anchorX        = Number(args.anchorX);
@@ -263,6 +300,14 @@ var P_2D_C = P_2D_C || {};
         }
 
         setupButton();
+    });
+
+    PluginManager.registerCommand('2D_Cat_CommEventPicBtn', 'destroyButtonById', args => {
+        destroyBtnById(Number(args.destroyButtonId));
+    });
+
+    PluginManager.registerCommand('2D_Cat_CommEventPicBtn', 'destroyAllButtons', () => {
+        destoryAllBtns();
     });
 
     function fixData() {
@@ -296,6 +341,7 @@ var P_2D_C = P_2D_C || {};
 
         button = new CommEventPicBtn();
 
+        button.id       = P_2D_C.btnId;
         button.commEvId = P_2D_C.commEventId;
 
         button.norTex = PIXI.Texture.from('img/pictures/' + P_2D_C.btnNorPicName + '.png');
@@ -306,8 +352,6 @@ var P_2D_C = P_2D_C || {};
 
         button.spr = new PIXI.Sprite(button.norTex);
         button.spr.texture     = button.norTex;
-        console.log(P_2D_C.anchorX)
-        console.log(P_2D_C.anchorY)
         button.spr.anchor.set(P_2D_C.anchorX, P_2D_C.anchorY);
         button.spr.x           = P_2D_C.posX;
         button.spr.y           = P_2D_C.posY;
@@ -322,12 +366,11 @@ var P_2D_C = P_2D_C || {};
         button.downBrit.brightness(P_2D_C.btnDownPicBrit, true);
 
         button.registerBtnEvents();
-        btnSprArr.push(button.spr);
         letHtmlTagsNotToBlockBtn();
         fixPlayerMove();
-        disableCallMenu();
 
-        SceneManager._scene.addChild(button.spr);
+        P_2D_C.picBtnsArr.push(button);
+        P_2D_C.picBtnContainer.addChild(button.spr);
     }
 
     function letHtmlTagsNotToBlockBtn() {
@@ -354,24 +397,52 @@ var P_2D_C = P_2D_C || {};
         $gamePlayer.setDirectionFix(false);
     }
 
-    function destoryAllBtnSpr() {
-        btnSprArr.forEach(e => {
-            SceneManager._scene.removeChild(e);
+    function destroyBtnById(id) {
+        P_2D_C.picBtnsArr.forEach(e => {
+            if (e.id === id) {
+                P_2D_C.picBtnContainer.removeChild(e.spr);
+                let eIdx = P_2D_C.picBtnsArr.indexOf(e);
+                if (eIdx > 0) P_2D_C.picBtnsArr.splice(eIdx, 1);
+            }
         });
+
+        if (P_2D_C.picBtnsArr.length <= 0) {
+            restoreHtmlTags();
+            restorePlayerMove();
+        }
+    }
+
+    function destoryAllBtns() {
+        P_2D_C.picBtnsArr.forEach(e => {
+            P_2D_C.picBtnContainer.removeChild(e.spr);
+        });
+        P_2D_C.picBtnsArr = [];
         restoreHtmlTags();
         restorePlayerMove();
-        restoreCallMenu();
     }
 
-    function disableCallMenu() {
-        Scene_Map.prototype.callMenu = () => {};
-    }
+    var _Scene_map_prototype_callMenu = Scene_Map.prototype.callMenu;
+    Scene_Map.prototype.callMenu = function() {
+        _Scene_map_prototype_callMenu.call(this);
 
-    function restoreCallMenu() {
-        Scene_Map.prototype.callMenu = _Scene_Map_prototype_callMenu;
-    }
+        if (P_2D_C.picBtnContainer)
+            P_2D_C.picBtnContainer.setParent(P_2D_C.pixiTempApp.stage);
+    };
 
-    /**
+    var _Scene_Map_prototype_onMapLoaded = Scene_Map.prototype.onMapLoaded;
+    Scene_Map.prototype.onMapLoaded = function() {
+        _Scene_Map_prototype_onMapLoaded.call(this);
+
+        if (P_2D_C.picBtnContainer) {
+            P_2D_C.picBtnContainer.setParent(SceneManager._scene._spriteset);
+            SceneManager._scene._spriteset.setChildIndex(P_2D_C.picBtnContainer, 2);
+        } else {
+            P_2D_C.picBtnContainer = new PIXI.Container();
+            SceneManager._scene._spriteset.addChildAt(P_2D_C.picBtnContainer, 2);
+        }
+    };
+
+    /***************************************************************************************
      * 下方代码摘自https://github.com/pixijs/pixijs/wiki/v5-Hacks#pixel-perfect-interaction，
      * 它实现了将Sprite作为按钮时，忽略透明像素的功能。
      */
