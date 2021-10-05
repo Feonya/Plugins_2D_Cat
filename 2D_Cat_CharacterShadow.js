c8202cd87637b25e161c578a2a5c49e32e31941c
tuxyin <tuxyin@163.com>
2021-10-05 10:57:24 +0800

    v1.2

--- 2D_Cat_CharacterShadow.js
+++ 2D_Cat_CharacterShadow.js
@@ -1,25 +1,34 @@
 /*:
  * @target     MZ
- * @plugindesc v1.1 为游戏角色添加自定义的阴影。
+ * @plugindesc v1.2 为游戏角色添加自定义的阴影。
  * @author     2D_猫
  * @url        https://space.bilibili.com/137028995
  *
  * @help
  * * 使用说明：
- * 1、在插件参数中设置好阴影文件，以及x、y轴偏移量后，进入游戏即可生效。
- * 2、在事件备注中写入[CharacterShadow hideShadow]可以隐藏本事件的阴影。
- * 3、在事件备注中写入[CharacterShadow shadowName:Shadow2 offsetX:0 offsetY:10]
+ * 1、仅主角、跟随者，以及图片名无感叹号“!”前缀的事件可以生成阴影。
+ * 2、所需阴影图片文件必须放置在img/system目录下。
+ * 3、在插件参数中设置好阴影文件，以及x、y轴偏移量后，进入游戏即可生效。
+ * 4、在事件备注中写入[CharacterShadow hideShadow]可以永久隐藏本事件的阴影。
+ * 5、在事件备注中写入[CharacterShadow shadowName:Shadow2 offsetX:0 offsetY:10]
  * 可以自定义本事件的阴影文件，以及x、y轴的偏移量。
- * 4、所需阴影图片文件必须放置在img/system目录下。
+ * 6、可在任意事件调用“禁用阴影”来暂时禁用主角、跟随者或指定事件的阴影。
+ * 7、可在任意事件调用“激活阴影”来重新激活主角、跟随者或指定事件的阴影（注意
+ * 本指令无法激活拥有[CharacterShadow hideShadow]标签事件的阴影）。
  *
  * * 使用条款：免费用于任何商业或非商业目的；允许在保留原作者信息的前提下修改代
  * 码；请在你的项目中致谢“2D_猫”，谢谢！:)
  *
  * * 更新日志：
+ * -- 20211005 v1.2
+ *     增加了“禁用阴影”和“激活阴影”插件指令。
  * -- 20210930 v1.1
  *     现在事件备注中的插件标签可以忽略大小写差异。
  * -- 20210928 v1.0
  *     实现插件基本功能。
+ *
+ * * 致谢说明：
+ * 1、感谢B站用户 C某人这次亿定 关于禁用主角阴影的建议。
  *
  * |\      /|          _
  * |-\____/-|         //
@@ -46,6 +55,26 @@
  * @text    y轴偏移
  * @type    string
  * @default 0
+ *
+ * @command disableShadow
+ * @text    禁用阴影
+ *
+ * @arg     disableEventId
+ * @text    事件编号
+ * @type    number
+ * @default 0
+ * @min     0
+ * @desc    待禁用阴影的事件编号，0表示主角和所有跟随者。
+ *
+ * @command enableShadow
+ * @text    激活阴影
+ *
+ * @arg     enableEventId
+ * @text    事件编号
+ * @type    number
+ * @default 0
+ * @min     0
+ * @desc    待激活阴影的事件编号，0表示主角和所有跟随者。
  */
 
 (() => {
@@ -61,6 +90,44 @@
     if (String(offsetY) === 'NaN') offsetY = 0;
     else if   (offsetY < -50000)   offsetY = -50000;
     else if   (offsetY > 50000)    offsetY = 50000;
+
+    PluginManager.registerCommand('2D_Cat_CharacterShadow', 'disableShadow', args => {
+        let eId = Number(args.disableEventId);
+        if (eId === 0) {
+            $gamePlayer._shadowTextureBackup  = $gamePlayer._shadowSprite.texture;
+            $gamePlayer._shadowSprite.texture = null;
+            let followers = $gamePlayer.followers()._data;
+            followers.forEach(e => {
+                e._shadowTextureBackup  = e._shadowSprite.texture;
+                e._shadowSprite.texture = null;
+            });
+        } else {
+            let event = $gameMap.event(eId);
+            if (!event._shadowSprite) return;
+            event._shadowTextureBackup = event._shadowSprite.texture;
+            event._shadowSprite.texture = null;
+        }
+    });
+
+    PluginManager.registerCommand('2D_Cat_CharacterShadow', 'enableShadow', args => {
+        let eId = Number(args.enableEventId);
+        if (eId === 0) {
+            if (!$gamePlayer._shadowTextureBackup) return;
+            $gamePlayer._shadowSprite.texture = $gamePlayer._shadowTextureBackup;
+            $gamePlayer._shadowTextureBackup  = null;
+            let followers = $gamePlayer.followers()._data;
+            followers.forEach(e => {
+                if (!e._shadowTextureBackup) return;
+                e._shadowSprite.texture = e._shadowTextureBackup;
+                e._shadowTextureBackup  = null;
+            });
+        } else {
+            let event = $gameMap.event(eId);
+            if (!event._shadowTextureBackup) return;
+            event._shadowSprite.texture = event._shadowTextureBackup;
+            event._shadowTextureBackup  = null;
+        }
+    });
 
     Game_Character.prototype.getCommentSettings = function() {
         let comment  = $gameMap.event(this.eventId()).event().note.trim();
@@ -123,7 +190,11 @@
         this._shadowSprite.anchor.x = 0.5;
         this._shadowSprite.anchor.y = 1;
         this._shadowSprite.z        = 0;
+
         tilemap.addChild(this._shadowSprite);
+        if (this._shadowTextureBackup) {
+            this._shadowSprite.texture = null;
+        }
     };
 
     Game_Character.prototype.updateShadow = function() {
@@ -147,7 +218,7 @@
         $gameMap.events().forEach(e => {
             e.updateShadow();
         });
-    }
+    };
 
     var _Spriteset_Map_prototype_createShadow = Spriteset_Map.prototype.createShadow;
     Spriteset_Map.prototype.createShadow = function() {
